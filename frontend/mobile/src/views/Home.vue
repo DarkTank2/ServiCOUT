48269d0b82126bf78c4a505bc4a77f37fd373688
ASeiler <e11771276@student.tuwien.ac.at>
2023-11-26 17:59:00 -0700

    adding home, onboarding, order and cash views

--- frontend/mobile/src/views/Home.vue
+++ frontend/mobile/src/views/Home.vue
@@ -1,5 +1,24 @@
 <template>
-  <HelloWorld />
+  <v-card>
+    <v-card-text>
+      <v-container fluid>
+        <v-row>
+          <v-col cols="12" sm="6">
+            <v-btn block :to="{ name: 'Order' }" outlined>
+              <v-icon>brunch_dining</v-icon>
+              <span class="ml-2">Bestellung aufnehmen</span>
+            </v-btn>
+          </v-col>
+          <v-col cols="12" sm="6">
+            <!-- <v-btn block :to="{ name: 'TableSelection' }" outlined>
+              <v-icon>euro_symbol</v-icon>
+              <span class="ml-2">Kassieren</span>
+            </v-btn> -->
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script lang="ts" setup>

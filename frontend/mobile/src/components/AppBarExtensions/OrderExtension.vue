24705c6a8803ba6c1063ae8d221dc5fb847e6ee5
ASeiler <e11771276@student.tuwien.ac.at>
2023-12-05 13:04:07 -1000

    adding order-extension, the slider for the categories

---
+++ frontend/mobile/src/components/AppBarExtensions/OrderExtension.vue
@@ -0,0 +1,5 @@
+<template></template>
+<script setup lang="ts">
+const { api } = useFeathers()
+const { data: categories } = toRefs(api.service('categories').findInStore(ref({ query: {} })))
+</script>

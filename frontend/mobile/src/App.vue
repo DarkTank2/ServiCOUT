b291de8238009cd59e28d763c03931be6b2a827a
ASeiler <e11771276@student.tuwien.ac.at>
2023-12-19 17:20:26 -1000

    adding border to shopping cart button to comply with general styling guidlines

--- frontend/mobile/src/App.vue
+++ frontend/mobile/src/App.vue
@@ -9,16 +9,10 @@
       <component v-if="meta.appBarComponent" :is="meta.appBarComponent" />
       <v-spacer v-if="meta.appBarComponent"></v-spacer>
       <template v-slot:extension v-if="meta.extension">
-        <component :is="meta.extension" :key="`${route.path}_extension`"/>
+        <component :is="meta.extension" :key="`${route.path}_extension`" />
       </template>
     </v-app-bar>
-    <v-bottom-sheet
-      v-model="bottomComponentModel"
-      v-if="meta.bottomComponent"
-      scrollable
-      max-height="80%"
-      persistent
-      >
+    <v-bottom-sheet v-model="bottomComponentModel" v-if="meta.bottomComponent" scrollable max-height="80%" persistent>
       <component :is="meta.bottomComponent" @closeBottomComponent="bottomComponentModel = false" />
     </v-bottom-sheet>
     <v-main>
@@ -27,25 +21,13 @@
           <component :is="Component" :key="route.path" />
         </transition>
       </router-view>
-      <v-btn
-            v-if="meta.bottomComponent"
-            elevation="2"
-            position="fixed"
-            style="bottom: 66px !important; right: 10px !important;"
-            class="text-none"
-            icon
-            @click="bottomComponentModel = true"
-            >
-            <v-badge
-              bordered
-              :content="badgeContent"
-              :model-value="badgeContent ? true : false"
-              :color="badgeColor"
-              offset-y="-8"
-              offset-x="-2"
-              >
-                <v-icon>mdi-cart-outline</v-icon>
-            </v-badge>
+      <v-btn v-if="meta.bottomComponent" elevation="2" position="fixed"
+        style="bottom: 66px !important; right: 10px !important; background: rgb(var(--v-theme-surface));" icon
+        @click="bottomComponentModel = true" variant="outlined">
+        <v-badge bordered :content="badgeContent" :model-value="badgeContent ? true : false" :color="badgeColor"
+          offset-y="-8" offset-x="-2">
+          <v-icon>mdi-cart-outline</v-icon>
+        </v-badge>
       </v-btn>
       <loading />
       <notification />
@@ -53,12 +35,12 @@
     <v-bottom-navigation color="primary" fixed grow v-if="$route.name !== 'Onboarding'">
       <v-btn :to="{ name: 'Order' }">
         <v-icon>mdi-silverware</v-icon>
-        
+
         <span>Bestellen</span>
       </v-btn>
       <v-btn :to="{ name: 'Cash/AllTables' }">
         <v-icon>mdi-cash-multiple</v-icon>
-        
+
         <span>Kassieren</span>
       </v-btn>
     </v-bottom-navigation>
@@ -66,51 +48,51 @@
 </template>
 
 <script lang="ts" setup>
-  import Notification from './components/Utilities/Notification.vue';
-  import Loading from './components/Utilities/Loading.vue';
+import Notification from './components/Utilities/Notification.vue';
+import Loading from './components/Utilities/Loading.vue';
 
-  const bottomComponentModel = ref(false)
-  
-  const authStore = useAuthStore()
-  authStore.authenticate({ strategy: 'local', email: 'email', password: 'password' })
-  const { api } = useFeathers()
-  const interval = setInterval(() => {
-    api.service('tenants').find({ query: { id: 2 } })
-    api.service('tables').find()
-    api.service('base-items').find()
-    api.service('sizes').find()
-    api.service('options').find()
-    api.service('flavours').find()
-    api.service('items').find()
-  }, 3000)
-  onBeforeUnmount(() => {
-    clearInterval(interval)
-  })
-    
-  const route = useRoute()
-  const meta = computed(() => {
-    return route.meta
-  })
-  const tenant = api.service('tenants').getFromStore(ref(2))
-  let rawOrderQuery = computed(() => {
-    return { query: { __isTemp: true }, temps: true }
-  })
-  const { data: rawOrder } = toRefs(api.service('ordered-items').findInStore(rawOrderQuery))
-  const allCartItemsAvailable = computed(() => {
-    return rawOrder.value.every(orderedItem => {
-      let item = api.service('items').findInStore({ query: { id: orderedItem.itemId! } }).data[0]
-      let baseItem = api.service('base-items').findInStore({ query: { id: item.baseItemId! } }).data[0]
-      return baseItem.available
-    })
-  })
-    
+const bottomComponentModel = ref(false)
 
-  const badgeContent = computed(() => {
-    return allCartItemsAvailable.value ? rawOrder.value.reduce((acc, val) => acc + val.quantity!, 0) : '!'
+const authStore = useAuthStore()
+authStore.authenticate({ strategy: 'local', email: 'email', password: 'password' })
+const { api } = useFeathers()
+const interval = setInterval(() => {
+  api.service('tenants').find({ query: { id: 2 } })
+  api.service('tables').find()
+  api.service('base-items').find()
+  api.service('sizes').find()
+  api.service('options').find()
+  api.service('flavours').find()
+  api.service('items').find()
+}, 3000)
+onBeforeUnmount(() => {
+  clearInterval(interval)
+})
+
+const route = useRoute()
+const meta = computed(() => {
+  return route.meta
+})
+const tenant = api.service('tenants').getFromStore(ref(2))
+let rawOrderQuery = computed(() => {
+  return { query: { __isTemp: true }, temps: true }
+})
+const { data: rawOrder } = toRefs(api.service('ordered-items').findInStore(rawOrderQuery))
+const allCartItemsAvailable = computed(() => {
+  return rawOrder.value.every(orderedItem => {
+    let item = api.service('items').findInStore({ query: { id: orderedItem.itemId! } }).data[0]
+    let baseItem = api.service('base-items').findInStore({ query: { id: item.baseItemId! } }).data[0]
+    return baseItem.available
   })
-  const badgeColor = computed(() => {
-    return allCartItemsAvailable.value ? 'green' : 'error'
-  })
+})
+
+
+const badgeContent = computed(() => {
+  return allCartItemsAvailable.value ? rawOrder.value.reduce((acc, val) => acc + val.quantity!, 0) : '!'
+})
+const badgeColor = computed(() => {
+  return allCartItemsAvailable.value ? 'green' : 'error'
+})
 </script>
 <style>
 .swipe-right-enter-active {
@@ -125,10 +107,12 @@
   transform: translateX(20px);
   opacity: 0;
 }
+
 .swipe-right-leave-to {
   transform: translateX(-20px);
   opacity: 0;
 }
+
 .swipe-left-enter-active {
   transition: all 0.5s ease-out;
 }
@@ -141,6 +125,7 @@
   transform: translateX(-20px);
   opacity: 0;
 }
+
 .swipe-left-leave-to {
   transform: translateX(20px);
   opacity: 0;

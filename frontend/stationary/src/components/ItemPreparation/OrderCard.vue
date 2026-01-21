fdd24d0d00bcb92b79d3275ab2e3949f010b9604
ASeiler <e11771276@student.tuwien.ac.at>
2026-01-21 13:08:16 +0100

    [stationary] changing font-size of order-card-title

--- frontend/stationary/src/components/ItemPreparation/OrderCard.vue
+++ frontend/stationary/src/components/ItemPreparation/OrderCard.vue
@@ -2,7 +2,7 @@
     <!-- <v-card @click.stop="claimOrder" :style="{ border: `thin solid ${claim === true ? 'red' : 'transparent'}`, display: 'flex', flexDirection: 'column' }" :height="cardHeight"> -->
     <v-card @click.stop="claimOrder" :style="{ border: `thin solid ${claim === true ? 'red' : 'transparent'}`, display: 'flex', flexDirection: 'column' }">
         <v-card-title>
-            <span :class="{ 'text-error': claim, 'text-success': !claim }">
+            <span :class="{ 'text-error': claim, 'text-success': !claim }" class="text-h4">
                 {{ title }}
             </span>
         </v-card-title>
